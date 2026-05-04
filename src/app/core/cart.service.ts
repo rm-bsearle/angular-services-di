ec1d26609ffdda2d3bab3b01879f42d374f3da5d
bsearle <briansearle@rocketmortgage.com>
2026-05-04 16:37:10 -0400

    use ng Signals for reactivity

--- src/app/core/cart.service.ts
+++ src/app/core/cart.service.ts
@@ -1,26 +1,23 @@
-import { Injectable } from "@angular/core";
-import { Product } from "@shared/product.model";
+import { computed, Injectable, signal } from '@angular/core';
+import { Product } from '@shared/product.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CartService {
-  private cart: Product[] = [];
+  public cart = signal<Product[]>([]);
 
   add(product: Product) {
-    this.cart.push(product);
+    this.cart.update((oldCart) => [...oldCart, product]);
   }
 
   remove(product: Product) {
-    this.cart = this.cart.filter(p => p !== product);
-  }
-
-  get cartItems() {
-    return this.cart;
+    this.cart.update(oldCart => oldCart.filter(p => p !== product));
   }
 
   get cartTotal() {
-    return this.cart.reduce((prev, next) => {
-      let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
-      return prev + next.price * discount;
-    }, 0);
+    return computed( () => this.cart().reduce((prev, next) => {
+        let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
+        return prev + next.price * discount;
+      }, 0)
+    )
   }
 }
