3eba0533aa8ed9b6ebc610338360608dadfc1470
bsearle <briansearle@rocketmortgage.com>
2026-05-12 11:11:21 -0400

    Create a configurabe cart service

--- src/app/core/cart.service.ts
+++ src/app/core/cart.service.ts
@@ -1,10 +1,26 @@
 import { computed, Injectable, InjectionToken, signal } from '@angular/core';
 import { Product } from '@shared/product.model';
 
+type CartOptions = {
+  persistanceType: string,
+  persistanceKey: string,
+}
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private cartItems = signal<Product[]>([]);
+  private cartOptions: CartOptions = {
+    persistanceType: 'local',
+    persistanceKey: 'cart',
+  }
+
+  constructor() {
+    if (this.cartOptions && this.cartOptions.persistanceType === 'local' ) {
+      const cartString = localStorage.getItem(this.cartOptions.persistanceKey);
+      const cart = cartString ? JSON.parse(cartString) as Product[] : [];
+      this.cartItems.set(cart);
+    }
+  }
 
   get cart() {
     return this.cartItems.asReadonly();
@@ -12,10 +28,21 @@ export class CartService {
 
   add(product: Product) {
     this.cartItems.update((oldCart) => [...oldCart, product]);
+    this.storeCart();
   }
 
   remove(product: Product) {
     this.cartItems.update(oldCart => oldCart.filter(p => p !== product));
+    this.storeCart();
+  }
+
+  private storeCart() {
+    if (this.cartOptions && this.cartOptions.persistanceType === 'local') {
+      localStorage.setItem(
+        this.cartOptions.persistanceKey,
+        JSON.stringify(this.cartItems())
+      );
+    }
   }
 
   get cartTotal() {
